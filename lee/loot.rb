5f53dd5245270368a291fc079eb269fe1bf45aed
Lee Quarella <lee@lucidfrog.com>
2015-05-22 10:47:24 -0400

    better looter

--- lee/loot.rb
+++ lee/loot.rb
@@ -1,14 +1,14 @@
 class Loot
-  attr_accessor :critter
   def initialize(critter)
-    critter
     loot critter
   end
 
   def loot critter
-    fput "search #{critter}"
+    fput "loot"
     (0..4).each do
       fput "get coin"
+      fput "stow gem"
+      fput "stow box"
     end
   end
 end
