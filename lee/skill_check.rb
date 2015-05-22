89547707066b9a25f9020fe4206156a80f757926
Lee Quarella <lee@lucidfrog.com>
2015-05-22 10:41:54 -0400

    better skill check

--- lee/skill_check.rb
+++ lee/skill_check.rb
@@ -12,4 +12,13 @@ class SkillCheck
       return false
     end
   end
+
+  def self.locked? skill
+    thing = dothis("exp #{skill}", Regexp.union([/lock/, /EXP HELP/]))
+    if(thing =~ /lock/)
+      return true
+    else
+      return false
+    end
+  end
 end
