ae2a69cf5926905307df34a109acf1467e61e4da
Lee Quarella <lee@lucidfrog.com>
2015-05-22 10:42:25 -0400

    cleaning

--- lee/skill_check.rb
+++ lee/skill_check.rb
@@ -1,18 +1,4 @@
 class SkillCheck
-  attr_reader :locked
-  def initialize(skill)
-    @locked = locked? skill
-  end
-
-  def locked? skill
-    thing = dothis("exp #{skill}", Regexp.union([/lock/, /EXP HELP/]))
-    if(thing =~ /lock/)
-      return true
-    else
-      return false
-    end
-  end
-
   def self.locked? skill
     thing = dothis("exp #{skill}", Regexp.union([/lock/, /EXP HELP/]))
     if(thing =~ /lock/)
