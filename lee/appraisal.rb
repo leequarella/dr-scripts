af9bd8024c34b4fc404c44487b8df502fa972d52
Lee Quarella <lee@lucidfrog.com>
2015-05-23 10:25:25 -0400

    improved combat appr

--- lee/appraisal.rb
+++ lee/appraisal.rb
@@ -1,6 +1,14 @@
 class Appraisal
-  def initialize
-    fput 'appr my leath'
+  COMBAT = ["leather", "foot", "stick", "spear", "buckler", "knu", "spike"]
+  def initialize(items=nil)
+    @items = items || COMBAT
+    @index = 0
+  end
+
+  def next
+    fput "appr my #{@items[@index]}"
+    @index += 1
+    @index = 0 if @index == COMBAT.length
     waitrt?
   end
 end
