3e62a3d9a7fa3d3c15d01cd9fcdf77913574c915
Lee Quarella <lee@lucidfrog.com>
2015-06-12 08:13:39 -0400

    improved loot and shoot

--- lee/loot.rb
+++ lee/loot.rb
@@ -1,17 +1,38 @@
 class Loot
+  GOT = Regexp.union([
+    /You pick up/,
+    /Stow what/,
+    /./,
+  ])
+
+  LOOT = Regexp.union([
+    /was carrying/,
+    /anything/
+  ])
+
   def initialize
     pause 1
-    fput "loot"
+    check = dothis "loot", LOOT
     waitrt?
-    fput "stow nug"
-    fput "stow box"
-    fput "stow card"
-    fput "stow dira"
-    (1..2).each do
-      fput "stow gem"
+    grab_stuff if check =~ /was carrying/
+  end
+
+  def grab_stuff
+    things = ['nug', 'box', 'card', 'dira', 'gem', 'coin']
+    things.each do |thing|
+      more = true
+      while more
+        more = grab thing
+      end
     end
-    (0..4).each do
-      fput "get coin"
+  end
+
+  def grab thing
+    check = dothis "stow #{thing}", GOT
+    if check =~ /Stow what/
+      false
+    else
+      true
     end
   end
 end
