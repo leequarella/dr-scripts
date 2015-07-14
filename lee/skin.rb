5c21b47252639770d083c5bfb4485452b3c7eacd
Lee Quarella <lee@lucidfrog.com>
2015-07-14 10:06:33 -0400

    arrange

--- lee/skin.rb
+++ lee/skin.rb
@@ -1,5 +1,22 @@
 class Skin
-  def initialize
+  ARRANGED = Regexp.union([
+    /You complete/,
+    /Roundtime/
+  ])
+
+  def initialize(arrange=nil)
+    arrange_critter# if arrange
+    skin
+  end
+
+  def arrange_critter
+    check = dothis "arr", ARRANGED
+    pause 0.2
+    waitrt?
+    arrange_critter if check =~ /Roundtime/
+  end
+
+  def skin
     fput "skin"
     waitrt?
   end
